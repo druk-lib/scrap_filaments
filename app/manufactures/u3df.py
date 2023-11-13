445798cdf998e478041b573fed9af088348c7792
igoroalex <igoroalex@gmail.com>
2023-11-13 18:48:51 +0200

    add U3DFSite

--- app/manufactures/u3df.py
+++ app/manufactures/u3df.py
@@ -3,15 +3,15 @@ from requests import RequestException
 
 from ..schemas import FilamentData, ManufacturerSite, ResponseSoup
 
-URL = "https://3dplast.biz"
+URL = 'https://u3df.com.ua'
 
 
-class ThreeDPlast(FilamentData):
+class U3DF(FilamentData):
     TYPES = {
-        "ABS": "ABS",
-        "PETg": "PETG",
-        "CoPET": "CoPET",
-        "PLA": "PLA",
+        'ABS': 'ABS',
+        'PETG': 'PETG',
+        'CoPET': 'CoPET',
+        'PLA': 'PLA',
     }
     GET_PAGE = True
 
@@ -25,7 +25,7 @@ class ThreeDPlast(FilamentData):
         return f'{URL}{self.card.find("a", class_="cs-goods-title").get("href")}'
 
     def get_name(self):
-        return self.card.find("a", class_="cs-goods-title").text
+        return self.card.find('a', class_='cs-goods-title').text
 
     def get_type(self):
         name = self.get_name()
@@ -40,33 +40,42 @@ class ThreeDPlast(FilamentData):
             for part in product_info.find_all('td', class_='b-product-info__cell'):
                 if 'Вага' in part.text:
                     return float(part.find_next('td', class_='b-product-info__cell').text.replace('кг', '').replace(',', '.').strip())
-        return 0.85
+        name_parts = self.get_name().replace('/', ' ').replace(')', ' ').split(' ')
+        if 'кг' in name_parts:
+            return float(name_parts[name_parts.index('кг') - 1].replace(',', '.'))
+        if 'грам' in name_parts:
+            return float(name_parts[name_parts.index('грам') - 1].replace(',', '.')) * 0.001 or float(name_parts[name_parts.index('грамм') - 1].replace(',', '.')) * 0.001
+        return 0.1
 
     def get_diameter(self):
+        if product_info := self.page.find('table', class_='b-product-info'):
+            for part in product_info.find_all('td', class_='b-product-info__cell'):
+                if 'Діаметр нитки' in part.text:
+                    return float(part.find_next('td', class_='b-product-info__cell').text.replace('мм', '').replace(',', '.').strip())
         return 1.75
 
     def get_color(self):
         if product_info := self.page.find('table', class_='b-product-info'):
             for part in product_info.find_all('td', class_='b-product-info__cell'):
                 if 'Колір' in part.text:
-                    return part.find_next('td', class_='b-product-info__cell').text
+                    return part.find_next('td', class_='b-product-info__cell').text.strip()
         return 'no color'
 
     def get_price(self):
         return float(
             self.card.find(
                 "span",
-                class_="cs-goods-price__value cs-goods-price__value_type_current",
+                class_="cs-goods-price__value",
             )
             .text.replace(u'\xa0', "")
             .replace("₴", "")
         )
 
 
-class ThreeDPlastSite(ManufacturerSite):
-    NAME = "3dplast"
-    FILTER = "/ua/g97296511-plastik-diametr-175"
-    FILAMENT = ThreeDPlast
+class U3DFSite(ManufacturerSite):
+    NAME = 'U3DF'
+    FILTER = '/ua/g21140449-nit?product_items_per_page=48&availability=availability'
+    FILAMENT = U3DF
 
     def get_filaments(self):
         try:
@@ -78,4 +87,4 @@ class ThreeDPlastSite(ManufacturerSite):
             logger.info(f'{self.NAME} - {URL}{self.FILTER} - {e}')
             return []
 
-        return bs.find_all("li", class_="cs-product-gallery__item")
+        return bs.find_all('li', class_='cs-product-gallery__item')
