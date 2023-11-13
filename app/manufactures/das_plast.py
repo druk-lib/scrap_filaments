87ee22854985f2e3ff5e556e6eb484e7f683a04d
igoroalex <igoroalex@gmail.com>
2023-11-13 20:10:37 +0200

    add DASPlastSite

---
+++ app/manufactures/das_plast.py
@@ -0,0 +1,92 @@
+from loguru import logger
+from requests import RequestException
+
+from ..schemas import FilamentData, ManufacturerSite, ResponseSoup
+
+URL = 'https://dasplast.com'
+
+
+class DASPlast(FilamentData):
+    TYPES = {
+        'АБС': 'ABS',
+        'PETG': 'PETG',
+        'CoPET': 'CoPET',
+        'PLA': 'PLA',
+    }
+    GET_PAGE = True
+
+    def miss(self):
+        good_state = self.card.find('span', class_='b-goods-data__state').text
+        # if (
+        #     'Немає в наявності' in good_state
+        #     or 'Під замовлення' in good_state
+        # ):
+        #     return True
+
+        return False
+
+    def get_url(self):
+        return f'{URL}{self.card.find("a", class_="b-goods-title").get("href")}'
+
+    def get_name(self):
+        return self.card.find('a', class_='b-goods-title').text
+
+    def get_type(self):
+        name = self.get_name()
+        for key, value in self.TYPES.items():
+            if key in name:
+                return value
+
+        return name
+
+    def get_weight(self):
+        if product_info := self.page.find('table', class_='b-product-info'):
+            for part in product_info.find_all('td', class_='b-product-info__cell'):
+                if 'Вага' in part.text:
+                    return float(part.find_next('td', class_='b-product-info__cell').text.replace('кг', '').replace(',', '.').strip())
+        return 0.1
+
+    def get_diameter(self):
+        if product_info := self.page.find('table', class_='b-product-info'):
+            for part in product_info.find_all('td', class_='b-product-info__cell'):
+                if 'Діаметр, мм' in part.text:
+                    return float(part.find_next('td', class_='b-product-info__cell').text.split(' ', '')[0].strip())
+        return 1.75
+
+    def get_color(self):
+        if product_info := self.page.find('table', class_='b-product-info'):
+            for part in product_info.find_all('td', class_='b-product-info__cell'):
+                if 'Колір' in part.text:
+                    return part.find_next('td', class_='b-product-info__cell').text.strip()
+        return 'no color'
+
+    def get_price(self):
+        try:
+            return float(
+                self.card.find(
+                    "span",
+                    class_="b-goods-price__value",
+                )
+                .text.replace(u'\xa0', "")
+                .replace("₴", "")
+            )
+        except Exception:
+            return 0.01
+
+
+class DASPlastSite(ManufacturerSite):
+    NAME = 'DASplast'
+    FILTER = '/ua/product_list?product_items_per_page=48'
+    FILAMENT = DASPlast
+
+    def get_filaments(self):
+        try:
+            bs = ResponseSoup(f'{URL}{self.FILTER}', self.NAME).get_response()
+        except RequestException:
+            logger.info(f'{self.NAME} - {URL}{self.FILTER} - RequestException')
+            return []
+        except Exception as e:
+            logger.info(f'{self.NAME} - {URL}{self.FILTER} - {e}')
+            return []
+
+        return bs.find_all('li', class_='b-product-gallery__item')
